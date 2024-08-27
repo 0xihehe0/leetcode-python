a08b7538d6f974fc25d95c5543f98ba8f8afee67
yaojinxi <864554492@qq.com>
2024-08-27 20:31:23 +0100

    creat 203

---
+++ leetcode/203/removeElements.py
@@ -0,0 +1,31 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-08-27 20:25:44
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-08-27 20:31:16
+FilePath: \leetcode\203\removeElements.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from typing import Optional  # Import Optional from the typing module
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+value = 4
+list1 = ListNode(1)
+n1 = ListNode(4)
+n2 = ListNode(4)
+n3 = ListNode(7)
+
+list1.next = n1
+n1.next = n2
+n2.next = n3
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        res = dummy = ListNode()
+        return
+    
+so = Solution()
+so.removeElements(list1, value)
