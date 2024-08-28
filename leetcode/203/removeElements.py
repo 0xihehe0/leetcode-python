d06534a392e23da46a2ad24bed5217139a2f2ed6
yaojinxi <864554492@qq.com>
2024-08-28 23:57:06 +0100

    finish 203

--- leetcode/203/removeElements.py
+++ leetcode/203/removeElements.py
@@ -2,7 +2,7 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-08-27 20:25:44
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-08-28 23:46:18
+LastEditTime: 2024-08-28 23:54:59
 FilePath: \leetcode\203\removeElements.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -24,15 +24,17 @@ n2.next = n3
 
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        # res = dummy = ListNode()
-        res = head.next
-        while res and res.next:
-            if res.val == val:
-                res.next = res.next.next
+        res = ListNode(0)
+        res.next = head
+        dummy = res
+        curr = head
+        while curr:
+            if curr.val == val:
+                dummy.next = curr.next
             else:
-                res = res.next
-                    
-        return res.next
+                dummy = curr 
+            curr = curr.next
+        return dummy.next
     
 so = Solution()
 so.removeElements(list1, value)
