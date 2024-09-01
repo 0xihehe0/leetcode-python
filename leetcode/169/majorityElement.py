3ebaecae9425940f5e79993356bcae4a60839ced
yaojinxi <864554492@qq.com>
2024-09-01 20:36:11 +0100

    finish another solution

--- leetcode/169/majorityElement.py
+++ leetcode/169/majorityElement.py
@@ -2,7 +2,7 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-09-01 17:37:53
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-09-01 19:22:20
+LastEditTime: 2024-09-01 20:34:46
 FilePath: \leetcode\169\majorityElement.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -55,7 +55,22 @@ class Solution:
         for key, val in majorMap.items():
             if val == max_value:
                 return key
+            
+    def majorityElement2(self, nums: List[int]) -> int:
+        count = 0
+        candidate = 0
+        for i in nums:
+            if count == 0:
+                candidate = i
+            if i == candidate:
+                count += 1
+            
+            else:
+                count -= 1
+                
+        return candidate
     
 so = Solution()
 
 print(so.majorityElement(nums))
+print(so.majorityElement2(nums))
