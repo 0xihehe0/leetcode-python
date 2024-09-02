53cbc0b6e04d21bf1445817d89d4e158ed1cca11
yaojinxi <864554492@qq.com>
2024-09-02 16:48:23 +0100

    creat 290

---
+++ leetcode/290/wordPattern.py
@@ -0,0 +1,11 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-02 16:47:58
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-02 16:48:19
+FilePath: \leetcode\290\wordPattern.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        return False
