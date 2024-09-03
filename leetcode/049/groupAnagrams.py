9ad004d472a724e0e43b694ca7d4256d864b49d8
yaojinxi <864554492@qq.com>
2024-09-03 14:06:34 +0100

    add 049

---
+++ leetcode/049/groupAnagrams.py
@@ -0,0 +1,19 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-03 14:05:18
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-03 14:05:37
+FilePath: \leetcode\049\groupAnagrams.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+
+'''
+from typing import List  # Import Optional from the typing module
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        return
+    
+so = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+result = so.groupAnagrams()
+print(result)
