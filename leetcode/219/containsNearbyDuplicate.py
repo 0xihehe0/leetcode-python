9690cb0cf6e93a9fa8a6033488298bf775825b3e
yaojinxi <864554492@qq.com>
2024-09-04 19:24:32 +0100

    finish 219

--- leetcode/219/containsNearbyDuplicate.py
+++ leetcode/219/containsNearbyDuplicate.py
@@ -2,9 +2,16 @@
 from typing import List  # Import Optional from the typing module
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic: dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dic and abs(i - dic[nums[i]]) <= k:
+                return True
+            dic[nums[i]] = i
+            
         return False
     
 so = Solution()
-nums = [1,2,3,1], k = 3
+nums = [1,2,3,1]
+k = 3
 result = so.containsNearbyDuplicate(nums, k)
 print(result)
