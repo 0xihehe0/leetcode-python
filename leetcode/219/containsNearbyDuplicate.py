d3dcb1ffcf4c4712782a8ed2f238f8038a5a98f2
yaojinxi <864554492@qq.com>
2024-09-04 18:11:47 +0100

    create 219

---
+++ leetcode/219/containsNearbyDuplicate.py
@@ -0,0 +1,10 @@
+
+from typing import List  # Import Optional from the typing module
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        return False
+    
+so = Solution()
+nums = [1,2,3,1], k = 3
+result = so.containsNearbyDuplicate(nums, k)
+print(result)
