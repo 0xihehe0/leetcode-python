fd609994d1ab8c539046465ab3d974d1073a147e
yaojinxi <864554492@qq.com>
2024-09-05 15:19:52 +0100

    create 392

---
+++ leetcode/392/isSubsequence.py
@@ -0,0 +1,16 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-05 15:18:23
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-05 15:19:35
+FilePath: \leetcode\392\isSubsequence.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        return False
+    
+so = Solution()
+s = "abc", t = "ahbgdc"
+result = so.isSubsequence(s,t)
+print(result)
