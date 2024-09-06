235446c7e273e2777f5075105f1ce9c2ae16ba60
yaojinxi <864554492@qq.com>
2024-09-06 19:16:36 +0100

    add 167 tips

--- leetcode/167/twoSum.py
+++ leetcode/167/twoSum.py
@@ -1,4 +1,16 @@
 from typing import List
+# Step 1:
+# I will initialize two pointers, first and last.
+# The first pointer will start at index 0, 
+# and the last pointer will start at the last index of the array (array length minus 1).
+
+# Step 2:
+# Then, I will calculate the sum of the values at these two pointers. 
+# If the sum is equal to the target value, 
+# I will return the indices of these pointers, 
+# each incremented by 1. If the sum is less than the target value, 
+# I will move the first pointer to the right (increment by 1). If the sum is greater than the target value, 
+# I will move the last pointer to the left (decrement by 1)."
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
