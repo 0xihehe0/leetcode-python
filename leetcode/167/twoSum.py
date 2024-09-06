7e0bf21d64285dc7d898d090209c71bc70c66406
yaojinxi <864554492@qq.com>
2024-09-06 17:46:15 +0100

    finish 167

--- leetcode/167/twoSum.py
+++ leetcode/167/twoSum.py
@@ -2,7 +2,18 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        first = 0
+        last = len(numbers) - 1
+        while first < last:
+            if numbers[first] + numbers[last] < target:
+                first +=1
+            
+            elif numbers[first] + numbers[last] > target:
+                last -= 1
+                
+            elif numbers[first] + numbers[last] == target:
+                return [first + 1, last + 1]
+
         return []
     
 numbers = [2,7,11,15]
