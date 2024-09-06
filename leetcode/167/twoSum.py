acf7cac79c4166fbf92be6ee10cb218db58b996c
yaojinxi <864554492@qq.com>
2024-09-06 16:30:53 +0100

    create 167

--- leetcode/167/twoSum.py
+++ leetcode/167/twoSum.py
@@ -2,5 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        return False
+        
+        return []
     
+numbers = [2,7,11,15]
+target = 9
+so = Solution()
+result = so.twoSum(numbers, target)
+
+print(result)
