e0a7d443defb259ec15300054821951d62f81d3e
yaojinxi <864554492@qq.com>
2024-09-06 19:53:30 +0100

    create 015

---
+++ leetcode/015/threeSum.py
@@ -0,0 +1,12 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-06 19:52:57
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-06 19:53:03
+FilePath: \leetcode\015\threeSum.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        return
