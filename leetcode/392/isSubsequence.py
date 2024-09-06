4481e42bd14eb692d556d3443a7502cb19e0ada6
yaojinxi <864554492@qq.com>
2024-09-06 12:06:27 +0100

    about 392 tips

--- leetcode/392/isSubsequence.py
+++ leetcode/392/isSubsequence.py
@@ -2,10 +2,17 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-09-05 15:18:23
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-09-06 11:36:49
+LastEditTime: 2024-09-06 12:06:17
 FilePath: \leetcode\392\isSubsequence.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+
+# "My approach is to initialize two pointers, index1 and index2, 
+# and then traverse the string t. index1 will follow the string t, 
+# and index2 will follow the string s. When the character at index1 in t is equal to the character at index2 in s, index2 will increment by 1.
+
+# If index2 reaches the length of the string s, return True; otherwise, return False."
+
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         index1 = 0
