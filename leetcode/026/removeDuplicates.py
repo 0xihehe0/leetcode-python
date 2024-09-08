e055caebad1a7e5ede4a8c2b00eb9dffa69a7093
yaojinxi <864554492@qq.com>
2024-09-08 14:38:28 +0100

    add 026 solution

--- leetcode/026/removeDuplicates.py
+++ leetcode/026/removeDuplicates.py
@@ -2,7 +2,7 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-09-08 13:58:13
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-09-08 14:15:42
+LastEditTime: 2024-09-08 14:36:47
 FilePath: \leetcode\026\removeDuplicates.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,8 +10,16 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        return
+        low = 1
+        fast = 1
+        while fast < len(nums):
+            if nums[fast] != nums[low - 1]:
+                nums[low] = nums[fast]
+                low += 1
+            
+            fast += 1
+        return low
         
-nums = [1,2,3,4,5]
+nums = [0,0,1,1,1,2,2,3,3,4]
 so = Solution()
 so.removeDuplicates(nums)
