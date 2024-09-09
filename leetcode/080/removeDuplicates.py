1ef5d74fcbb6e0db2049f95aabeb2f7056a97098
yaojinxi <864554492@qq.com>
2024-09-09 16:16:06 +0100

    add 080 tips

--- leetcode/080/removeDuplicates.py
+++ leetcode/080/removeDuplicates.py
@@ -1,3 +1,18 @@
+
+# "I will use a two-pointer approach to resolve the problem.
+
+# Step 1:
+# I initialize two variables, index and occurrence, both set to 1. Then, we start traversing the input nums array from index 1 to the end.
+
+# Step 2:
+# If nums[i] is equal to nums[i - 1], increment the occurrence by 1. Otherwise, reset occurrence to 1.
+
+# Step 3:
+# If occurrence is less than or equal to 2, set nums[index] to nums[i], and then increment index by 1.
+
+# Step 4:
+# Finally, return the index."
+
 from typing import List
 
 class Solution:
