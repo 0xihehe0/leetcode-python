9e2b4f18f9740be15ad34b61ef42ed46812e48ab
yaojinxi <864554492@qq.com>
2024-09-09 13:31:49 +0100

    add 189 tips

--- leetcode/189/rotate.py
+++ leetcode/189/rotate.py
@@ -2,12 +2,21 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-03-27 22:38:55
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-09-09 13:30:33
+LastEditTime: 2024-09-09 13:31:42
 FilePath: \leetcode\189\rotate.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
+# "My approach is to use the two-pointer technique to reverse the input array in order to solve the problem.
 
+# Step 1:
+# First of all, we need to reverse the entire nums array.
+
+# Step 2:
+# Then, we need to reverse the front part of the array, which is the part from index 0 to k - 1.
+
+# Step 3:
+# Finally, we reverse the back part, which is the part from index k to the end of the array."
 
 class Solution:
     def rotate(self, nums, k) -> None:
