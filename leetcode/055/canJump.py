467b51a1d1bb6e9f729ee2efcd6e33177d1c8a5e
yaojinxi <864554492@qq.com>
2024-09-09 16:31:30 +0100

    add 055

---
+++ leetcode/055/canJump.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        print(nums)
+        return False
+
+nums = [2,3,1,1,4]
+so = Solution()
+result = so.canJump(nums)
+print(result)
