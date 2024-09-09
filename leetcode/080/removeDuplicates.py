0bdd0c326be87f0c40c6878b9df36364e222930d
yaojinxi <864554492@qq.com>
2024-09-09 15:35:08 +0100

    finish 080

--- leetcode/080/removeDuplicates.py
+++ leetcode/080/removeDuplicates.py
@@ -2,8 +2,23 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        return
+        index = 1
+        occurance = 1
+        
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                occurance += 1
+            else:
+                occurance = 1
+                
+            if occurance <= 2:
+                nums[index] = nums[i]
+                index += 1
+            
+        return index
         
         
 nums = [1,1,1,2,2,3]
-result = Solution()
+so = Solution()
+result = so.removeDuplicates(nums)
+print(result)
