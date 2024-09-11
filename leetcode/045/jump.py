c6fe6e4d3cbd72d25a2fd099a36496513cb4d07e
yaojinxi <864554492@qq.com>
2024-09-11 13:26:33 +0100

    create 045

---
+++ leetcode/045/jump.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        return 0
+    
+nums = [2,3,1,1,4]
+so = Solution()
+result = so.jump(nums)
+print(result)
