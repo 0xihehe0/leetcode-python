c56fd6768c1bcc67aa88fb62924bf3e787a1a389
yaojinxi <864554492@qq.com>
2024-09-11 15:37:17 +0100

    add 045 mistake

--- leetcode/209/minSubArrayLen.py
+++ leetcode/209/minSubArrayLen.py
@@ -2,6 +2,23 @@ from typing import List
 
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        right = 1
+        while right < len(nums):
+            print(left,right)
+            if nums[left] or nums[right] == target:
+                return 1
+            
+            if nums[left] + nums[right] == target:
+                print(left,right)
+            
+            if nums[left] + nums[right] < target:
+                right += 1
+                
+            if nums[left] + nums[right] > target:
+                left += 1
+                right += 1
+                
         return
         
 target = 7 
