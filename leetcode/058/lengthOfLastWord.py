3aa448fc268720f82f722c2d5c82b6ab6f67da9a
yaojinxi <864554492@qq.com>
2024-09-11 17:34:04 +0100

    finish 058 solution

--- leetcode/058/lengthOfLastWord.py
+++ leetcode/058/lengthOfLastWord.py
@@ -1,8 +1,22 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return 
-    
-s = "Hello World"
+        if len(s) == 0:
+            return 0
+        type = False
+        index = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != " ":
+                type = True
+                
+            if s[i] == " " and index > 0:
+                return index
+            
+            if type == True:
+                index +=1
+        
+        return index
+     
+s = "   fly me   to   the moon  "
 so = Solution()
-result = so.lengthOfLastWord()
+result = so.lengthOfLastWord(s)
 print(result)
