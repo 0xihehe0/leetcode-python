7a65781202254ca7bc305c0c469705e4756e24a3
yaojinxi <864554492@qq.com>
2024-09-11 14:13:44 +0100

    finish 045 solution

--- leetcode/045/jump.py
+++ leetcode/045/jump.py
@@ -2,7 +2,19 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        return 0
+        far = 0
+        near = 0
+        jumpIndex = 0
+        length = len(nums)
+        while far < length - 1: 
+            fastherst = 0
+            for i in range(near,far + 1):
+                fastherst = max(fastherst,i + nums[i])
+                
+            near = far + 1       
+            far = fastherst
+            jumpIndex += 1
+        return jumpIndex
     
 nums = [2,3,1,1,4]
 so = Solution()
