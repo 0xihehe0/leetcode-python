ac87278179ef1cacc7216c0c8b7b484e7eb4b563
yaojinxi <864554492@qq.com>
2024-09-11 19:52:04 +0100

    add 058 notice

--- leetcode/058/lengthOfLastWord.py
+++ leetcode/058/lengthOfLastWord.py
@@ -1,20 +1,27 @@
+# "First of all, we decide to iterate through 
+# the input string from the end. If the current character is a space, we skip it. 
+# If it's not a space, we set a Boolean flag to True. 
+# As long as the Boolean flag is True, 
+# we increment the index value in each iteration until we encounter the next space. 
+# Finally, we calculate the total index and return it."
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         if len(s) == 0:
             return 0
-        type = False
+        
+        foundWord = False
         index = 0
+        
         for i in range(len(s) - 1, -1, -1):
             if s[i] != " ":
-                type = True
-                
-            if s[i] == " " and index > 0:
+                foundWord = True
+                index += 1
+            elif foundWord:
                 return index
-            
-            if type == True:
-                index +=1
         
         return index
+
      
 s = "   fly me   to   the moon  "
 so = Solution()
