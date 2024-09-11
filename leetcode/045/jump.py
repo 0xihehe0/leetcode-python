199c44792dc40021b0fce725a0947bbcd4d4c5f6
yaojinxi <864554492@qq.com>
2024-09-11 14:37:37 +0100

    create 209

--- leetcode/045/jump.py
+++ leetcode/045/jump.py
@@ -1,3 +1,11 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-11 13:20:07
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-11 14:33:42
+FilePath: \leetcode\045\jump.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 # step1:
 # First of all,we initialize three variates is near and far and jumpIndex,
 
---
+++ leetcode/209/minSubArrayLen.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        return
+        
+target = 7 
+nums = [2,3,1,2,4,3]
+so = Solution()
+result = so.minSubArrayLen(target,nums)
+print(result)
