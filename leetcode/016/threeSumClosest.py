895cb0c5f2e2df34d8a6d91a323163f5767723d2
yaojinxi <864554492@qq.com>
2024-09-12 14:15:35 +0100

    create 016

---
+++ leetcode/016/threeSumClosest.py
@@ -0,0 +1,13 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-12 14:15:14
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-12 14:15:28
+FilePath: \leetcode\016\threeSumClosest.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from typing import List
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        return
