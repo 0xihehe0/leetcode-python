6476d96c3ba14d33e0074153fe7136ddb7f42736
yaojinxi <864554492@qq.com>
2024-09-12 13:04:41 +0100

    add 005 solution

--- leetcode/005/longestPalindrome.py
+++ leetcode/005/longestPalindrome.py
@@ -2,10 +2,15 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-09-12 12:37:56
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-09-12 12:39:22
+LastEditTime: 2024-09-12 13:04:31
 FilePath: \leetcode\005\longestPalindrome.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         return
+    
+s="absc"
+so = Solution()
+result = so.longestPalindrome(s)
+print(result)
