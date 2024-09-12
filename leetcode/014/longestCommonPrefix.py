2e43b16e030304e9083652a791df27ffef27db4b
yaojinxi <864554492@qq.com>
2024-09-12 10:34:55 +0100

    create 014

---
+++ leetcode/014/longestCommonPrefix.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        return
+    
+strs = ["flower","flow","flight"]
+so = Solution()
+result = so.longestCommonPrefix(strs)
+print(result)
