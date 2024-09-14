e63a2c0ce1f66d4a457951d91a569175bb95ae3c
yaojinxi <864554492@qq.com>
2024-09-14 11:14:51 +0100

    add 075 solutions

--- leetcode/075/sortColors.py
+++ leetcode/075/sortColors.py
@@ -2,7 +2,7 @@
 Author: yaojinxi 864554492@qq.com
 Date: 2024-03-06 19:19:30
 LastEditors: yaojinxi 864554492@qq.com
-LastEditTime: 2024-03-06 21:36:35
+LastEditTime: 2024-09-14 11:14:42
 FilePath: \leetcode\075\sortColors.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -31,10 +31,31 @@ class Solution(object):
             elif nums[i] == 1:
                 nums[p1], nums[i] = nums[i], nums[p1]
                 p1 += 1
+                
+#     Step 1:
+# I will create a dictionary to store the frequency of each color in the input nums array.
+
+# Step 2:
+# I will update the array by first getting the frequency of each color and initializing a variable called index.
+
+# Step 3:
+# Increment the index by the frequency to move to the next segment of the array for the next color.            
+                
+    def sortColors3(self,nums):
+        count = {}
+        for i in range(len(nums)):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            
+        index = 0
+        for color in range(3):
+            freq = count.get(color, 0)
+            nums[index : index + freq] = [color] * freq
+            index += freq
                     
     
     
 nums = [2,1,1,0,1,2]
 so = Solution()
-print(so.sortColors(nums))
-print(so.sortColors2(nums))
+# print(so.sortColors(nums))
+# print(so.sortColors2(nums))
+print(so.sortColors3(nums))
