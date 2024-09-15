81730847bc653c9177840a7bc6ecfdf7e81e13d0
yaojinxi <864554492@qq.com>
2024-09-15 14:40:02 +0100

    create 086

---
+++ leetcode/086/partition.py
@@ -0,0 +1,24 @@
+'''
+Author: yaojinxi 864554492@qq.com
+Date: 2024-09-15 14:38:45
+LastEditors: yaojinxi 864554492@qq.com
+LastEditTime: 2024-09-15 14:39:47
+FilePath: \leetcode\086\partition.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from typing import Optional  # Import Optional from the typing module
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        return
+    
+list1 = ListNode(1)
+n1 = ListNode(4)
+n2 = ListNode(7)
+n3 = ListNode(2)
+n4 = ListNode(2)
